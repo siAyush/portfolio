cdd40248b6f7a8a9ade3df68b017bf6912105e10
Ayush <siayush.gupta@gmail.com>
2024-06-29 00:20:41 +0530

    style fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -17,11 +17,11 @@ const BLUR_FADE_DELAY = 0.04;
 export default function Page() {
   return (
     <main className="flex flex-col min-h-[100dvh] space-y-10">
+      <BlurFade delay={BLUR_FADE_DELAY}>
+        <NavBar></NavBar>
+      </BlurFade>
       <section id="hero">
         <div className="mx-auto w-full max-w-2xl space-y-8">
-          <BlurFade delay={BLUR_FADE_DELAY}>
-            <NavBar></NavBar>
-          </BlurFade>
           <div className="gap-2 flex justify-between">
             <div className="flex-col flex flex-1 space-y-1.5">
               <BlurFadeText
