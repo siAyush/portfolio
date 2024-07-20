9d8995366e2293c6c3d7559a0b43208dded72346
Ayush <siayush.gupta@gmail.com>
2024-07-20 17:00:32 +0530

    blogs

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,37 @@
+import { getPost } from "@/data/blog";
+import { formatDate } from "@/lib/utils";
+import { notFound } from "next/navigation";
+import { Suspense } from "react";
+
+export default async function Blog({
+  params,
+}: {
+  params: {
+    slug: string;
+  };
+}) {
+  let post = await getPost(params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  return (
+    <section id="blog">
+      <h1 className="title font-medium text-2xl tracking-tighter max-w-[650px]">
+        {post.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm max-w-[650px]">
+        <Suspense fallback={<p className="h-5" />}>
+          <p className="text-sm text-neutral-600 dark:text-neutral-400">
+            {formatDate(post.metadata.publishedAt)}
+          </p>
+        </Suspense>
+      </div>
+      <article
+        className="prose dark:prose-invert"
+        dangerouslySetInnerHTML={{ __html: post.source }}
+      ></article>
+    </section>
+  );
+}
