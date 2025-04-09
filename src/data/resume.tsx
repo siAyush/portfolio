e4469ecdde76b76bec6c512c624a69b69ebd3586
Ayush <siayush.gupta@gmail.com>
2025-04-09 10:38:05 +0530

    ui update

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -49,7 +49,7 @@ export const DATA = {
       location: "Remote",
       title: "Software Engineer",
       logoUrl: "/atomic.png",
-      start: "Dec 2022",
+      start: "Dec 2021",
       end: "Present",
       description:
         "Implemented server-side applications using Node.js, ensuring efficient handling of backend logic. Collaborated closely with the frontend team to seamlessly integrate APIs into the frontend, ensuring smooth functionality and user experience across the application.  Implemented micro frontend architecture, enabling modular development and seamless integration of multiple frontend applications for improved scalability and maintainability.  Increased code coverage from 40% to 82% by writing comprehensive unit and integration tests, ensuring greater reliability and maintainability. Led the integration of Google Tag Manager (GTM) for improved tracking and analytics. Worked with AWS to deploy and scale applications",
